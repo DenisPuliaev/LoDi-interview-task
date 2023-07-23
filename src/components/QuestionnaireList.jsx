b15ce4962ae6a763fdcc66d7707804928f02ac8c
DenisPuliaev <denis.puliaev@gmail.com>
2023-07-23 22:42:05 +0300

    clean files and fix file extensions

--- src/components/QuestionnaireList.jsx
+++ src/components/QuestionnaireList.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useState } from "react";
+import React from "react";
 import { ReactComponent as OtherIcon } from "../assets/questionnaire/other.svg";
 import { RadioGroup } from "@headlessui/react";
 import { iconNameToIcon } from "./questionnaire";
